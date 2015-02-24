2479ee5632ba222126fb974d160c2d4b51b26992
cristianprofile <cristianprofile@gmail.com>
2015-02-23 23:18:23 -0500

    @SpringBootApplication use to autoconfig test without others annotations, clear and best solution to test spring data jpa repositories

--- mylab-core/mylab-core-repository/src/test/java/com/mylab/cromero/TestRepositoryConfigIT.java
+++ mylab-core/mylab-core-repository/src/test/java/com/mylab/cromero/TestRepositoryConfigIT.java
@@ -1,5 +1,6 @@
-package com.mylab.cromero.repository;
+package com.mylab.cromero;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
@@ -9,13 +10,10 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.mylab.cromero.domain.User;
+import com.mylab.cromero.repository.UserRepository;
 
 @Configuration
-@EnableJpaRepositories(basePackageClasses = UserRepository.class)
-@EntityScan(basePackageClasses = User.class)
-@ComponentScan("com.mylab")
-@Import({ DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class })
+@SpringBootApplication
 public class TestRepositoryConfigIT {
 
 }
