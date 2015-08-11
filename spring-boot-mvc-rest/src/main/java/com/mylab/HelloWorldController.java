63c0c59fce50fcec76476596bc010728a8fd12ea
cristianprofile <cristianprofile@gmail.com>
2015-08-11 10:21:16 -0500

    Remove unused imports

--- spring-boot-mvc-rest/src/main/java/com/mylab/HelloWorldController.java
+++ spring-boot-mvc-rest/src/main/java/com/mylab/HelloWorldController.java
@@ -1,7 +1,9 @@
 package com.mylab;
 
-import java.util.List;
-
+import com.mylab.cromero.dto.BaseRequest;
+import com.mylab.cromero.dto.BaseResponse;
+import com.mylab.cromero.exception.BaseNotFoundException;
+import com.mylab.cromero.service.BaseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,18 +11,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.mylab.cromero.dto.BaseRequest;
-import com.mylab.cromero.dto.BaseResponse;
-import com.mylab.cromero.exception.BaseNotFoundException;
-import com.mylab.cromero.service.BaseService;
+import java.util.List;
 
 /**
  * Rest controller pizzas example.
