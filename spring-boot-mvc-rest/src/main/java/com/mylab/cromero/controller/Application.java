1b5dc4145eca56e8dc3ff95609aa9addc15bb860
Cristian Romero Matesanz <cristianprofile@gmail.com>
2016-07-14 17:41:13 +0200

    Updated Component scan

--- spring-boot-mvc-rest/src/main/java/com/mylab/cromero/controller/Application.java
+++ spring-boot-mvc-rest/src/main/java/com/mylab/cromero/controller/Application.java
@@ -29,7 +29,7 @@ import java.util.Arrays;
  */
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan(basePackageClasses = Application.class)
+@ComponentScan(basePackageClasses = HelloWorldController.class)
 @Import({ConfigurationService.class})
 public class Application {
 
